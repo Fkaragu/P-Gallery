82346a84707c8fd8cba7102ee82efc58b956d099
Francis Karagu <fkaragu@gmail.com>
2019-03-01 10:45:50 +0300

    Added a function on location and category to return name

--- holder/models.py
+++ holder/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Location(models.Model):
   name = models.CharField(max_length =60)
 
+  def __str__(self):
+      return self.name
+
 class Category(models.Model):
    name = models.CharField(max_length =60)
+
+   def __str__(self):
+       return self.name
 
 class Image(models.Model):
    name = models.CharField(max_length =60)
