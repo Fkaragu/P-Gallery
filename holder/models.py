cb99a3d4f7039e83f2c9cee3830036cce5b8c701
Francis Karagu <fkaragu@gmail.com>
2019-03-01 10:25:26 +0300

    Added models

--- holder/models.py
+++ holder/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class Image(models.Model):
+   name = models.CharField(max_length =60)
+   description = models.TextField()
+   location = models.ForeignKey(Location)
+   category = models.ForeignKey(Category)
+   pub_date = models.DateTimeField(auto_now_add=True)
+   article_image = models.ImageField(upload_to = 'articles/')
+
+class Location(models.Model):
+  name = models.CharField(max_length =60)
+
+class Category(models.Model):
+   name = models.CharField(max_length =60)
