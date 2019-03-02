7a2873ed95ec5487ae73351ac43672a9a9f71ea0
Francis Karagu <fkaragu@gmail.com>
2019-03-02 23:23:22 +0300

    Added styling and images on index page

--- holder/models.py
+++ holder/models.py
@@ -6,11 +6,29 @@ class Location(models.Model):
   def __str__(self):
       return self.name
 
+  def save_location(self):
+      self.save()
+
+  def update_location(self):
+      self.update()
+
+  def delete_location(self):
+      self.delete()
+
 class Category(models.Model):
    name = models.CharField(max_length =60)
 
    def __str__(self):
        return self.name
+
+   def save_category(self):
+       self.save()
+
+   def update_category(self):
+       self.update()
+
+   def delete_category(self):
+       self.delete()
 
 class Image(models.Model):
    name = models.CharField(max_length =60)
@@ -19,3 +37,30 @@ class Image(models.Model):
    category = models.ForeignKey(Category)
    pub_date = models.DateTimeField(auto_now_add=True)
    article_image = models.ImageField(upload_to = 'articles/')
+
+   def __str__(self):
+       return self.name
+
+   def save_image(self):
+       self.save()
+
+   def update_image(self):
+       self.update()
+
+   def delete_image(self):
+       self.delete()
+
+   @classmethod
+   def get_image_by_id(cls,id):
+       img = cls.objects.filter(id__id = id)
+       return img
+
+   @classmethod
+   def search_by_image(cls,search_cat):
+       img = cls.objects.filter(article_image__icontains=search_cat)
+       return img
+
+   @classmethod
+   def filter_by_location(cls,id):
+       img = cls.objects.filter(location__location = id)
+       return img
