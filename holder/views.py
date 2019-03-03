394d645973ebd0a7ab55841fa5276c467d75a383
Francis Karagu <fkaragu@gmail.com>
2019-03-03 12:02:46 +0300

    Added file structure for post to pick details

--- holder/views.py
+++ holder/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
 
 def post_view(request):
-    return render(request, 'post.html')
+    img = Image.all_images()
+    return render(request, 'post.html',{"img":img})
