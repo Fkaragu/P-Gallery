c969b837c82a5db6541a145e15c41a75c5166a41
Francis Karagu <fkaragu@gmail.com>
2019-03-04 15:07:40 +0300

    Added search function

--- holder/views.py
+++ holder/views.py
@@ -9,3 +9,16 @@ def welcome(request):
 def post_view(request):
     img = Image.all_images()
     return render(request, 'post.html',{"img":img})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"category": searched_category})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
